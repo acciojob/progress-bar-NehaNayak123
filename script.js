016ab3322290ac0b7c13c398acbbe073e4fc5e71
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-04 23:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//your JS code here. If required.
+const progress=document.getElementById("progress")
+const prev=document.getElementById("prev")
+const next=document.getElementById("next")
+const circles=document.querySelectorAll(".circle");
+
+let curr=1;
+
+next.addEventListener("click", ()=>{
+    curr++;
+    // console.log(curr)
+    if(curr>circles.length){
+        curr=circles.length
+    }
+    render()
+})
+prev.addEventListener("click", ()=>{
+    curr--;
+    console.log(curr)
+    if(curr<1){
+        curr=1;
+    }
+    render()
+})
+function render(){
+    circles.forEach((circle, idx)=>{
+        if(idx<curr){
+            circle.classList.add('active');
+        }else{
+            circle.classList.remove('active');
+        }
+    })
+
+    if(curr===1){
+        prev.disabled=true;
+    }else{
+        prev.disabled=false;
+    }
+
+    if(curr===circles.length){
+        next.disabled=true;
+    }else{
+        next.disabled=false;
+    }
+}
